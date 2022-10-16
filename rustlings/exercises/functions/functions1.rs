a82d8217595928c1b1572b41b4f45feb2c45d309
Ever Ruiz Diaz <ruizdiaz.oe@gmail.com>
2022-10-16 18:25:01 +0200

    Deleted .git from rustlings

---
+++ rustlings/exercises/functions/functions1.rs
@@ -0,0 +1,11 @@
+// functions1.rs
+// Execute `rustlings hint functions1` or use the `hint` watch subcommand for a hint.
+
+
+fn call_me() {
+    let name = "Ever".to_string();
+}
+
+fn main() {
+    call_me();
+}
